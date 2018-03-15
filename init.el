2abff7427107593ff617c4535c1003b62014d4d5
Manoj321 <manojm.321@gmail.com>
2018-03-15 09:52:11 -0700

    Some cleanup

--- init.el
+++ init.el
@@ -1,13 +1,7 @@
-;; Turn off GUI, because GUI makes you slow.
-(when window-system
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (tooltip-mode -1))
-
 ;; Make sure melpa is added to package-archives.
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (package-initialize)
 
 ;; Install use package.
@@ -18,9 +12,11 @@
 ;; From use-package readme.
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
-(require 'netrc)
+
+(use-package diminish
+  :ensure t)
+(use-package bind-key
+  :ensure t)
 
 ;; So that Arista specific .el files are loaded.
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
@@ -43,15 +39,17 @@
     ("38e64ea9b3a5e512ae9547063ee491c20bd717fe59d9c12219a0b1050b439cdd" default)))
  '(gud-gdb-command-name "gdb --annotate=1")
  '(highlight-indent-guides-method (quote character))
- '(kubernetes-logs-arguments (quote ("--tail=500")) t)
+ '(large-file-warning-threshold nil)
  '(package-selected-packages
    (quote
-    (hierarchy redditor redditor-mode redittor-mode redittor org-gcal tree-mode htmlize org-mu4e mu4e-alert mu4e js2-mode indium fontawesome ess adaptive-wrap ac-dabbrev pdf-tools go-guru go-autocomplete graphviz-dot-mode tickscript-mode ob-http pdb-mode undo-tree kubernetes yaml-mode waher-theme use-package solarized-theme smartscan smartparens org-journal ob-go monokai-theme markdown-mode magit ledger-mode jedi highlight-indent-guides helm-swoop helm-company gotest flymd flycheck fill-column-indicator exec-path-from-shell elfeed-web elfeed-org dockerfile-mode docker cyberpunk-theme ace-jump-mode))))
+    (kubernetes-el org-web-tools org-bullets hierarchy redditor redditor-mode redittor-mode redittor org-gcal tree-mode htmlize org-mu4e mu4e-alert mu4e js2-mode indium fontawesome ess adaptive-wrap ac-dabbrev pdf-tools go-guru go-autocomplete graphviz-dot-mode tickscript-mode ob-http pdb-mode undo-tree kubernetes yaml-mode waher-theme use-package solarized-theme smartscan smartparens org-journal ob-go monokai-theme markdown-mode magit ledger-mode jedi highlight-indent-guides helm-swoop helm-company gotest flymd flycheck fill-column-indicator exec-path-from-shell elfeed-web elfeed-org dockerfile-mode docker cyberpunk-theme ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elfeed-search-title-face ((t (:foreground "#c678dd" :height 1.2))))
+ '(go-guru-hl-identifier-face ((t (:underline t))))
  '(highlight ((t (:background "color-237"))))
  '(highlight-indent-guides-character-face ((t (:foreground "gray73"))))
  '(ledger-font-xact-highlight-face ((t (:background "color-237"))))
@@ -68,4 +66,5 @@
  '(magit-diff-our ((t nil)))
  '(magit-diff-removed ((t (:foreground "#aa2222"))))
  '(magit-diff-removed-highlight ((t (:foreground "#aa2222"))))
- '(magit-section-highlight ((t nil))))
+ '(magit-section-highlight ((t nil)))
+ '(quote ((org-block-begin-line ((t (:background "gray22" :foreground "gray50" :height 0.9)))) (org-block-end-line ((t (:background "gray22" :foreground "gray50" :height 0.9)))))))
