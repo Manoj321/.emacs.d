602f92961a6d9b1dd44ca1d4d2ce32ae74f779c8
Manoj321 <manojm.321@gmail.com>
2018-10-13 10:54:18 -0700

    Add melpa-stable to package-archives

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;; Make sure melpa is added to package-archives.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
