b0425559b8cfd6ce8ff6a7079c252187c6a16cd6
manojm321 <manojm321@arista.com>
2019-07-21 15:23:39 -0700

    Misc changes

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
 ;; load secrets
-(load-library "~/secrets.el.gpg")
+(load-library "~/secrets.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -42,10 +42,11 @@
     ("38e64ea9b3a5e512ae9547063ee491c20bd717fe59d9c12219a0b1050b439cdd" default)))
  '(gud-gdb-command-name "gdb --annotate=1")
  '(highlight-indent-guides-method (quote character))
+ '(indent-tabs-mode nil)
  '(large-file-warning-threshold nil)
  '(package-selected-packages
    (quote
-    (kubernetes-el org-web-tools org-bullets hierarchy redditor redditor-mode redittor-mode redittor org-gcal tree-mode htmlize org-mu4e mu4e-alert mu4e js2-mode indium fontawesome ess adaptive-wrap ac-dabbrev pdf-tools go-guru go-autocomplete graphviz-dot-mode tickscript-mode ob-http pdb-mode undo-tree kubernetes yaml-mode waher-theme use-package solarized-theme smartscan smartparens org-journal ob-go monokai-theme markdown-mode magit ledger-mode jedi highlight-indent-guides helm-swoop helm-company gotest flymd flycheck fill-column-indicator exec-path-from-shell elfeed-web elfeed-org dockerfile-mode docker cyberpunk-theme ace-jump-mode))))
+    (vline-mode yasnippet-snippets company-jedi kubernetes-el org-web-tools org-bullets hierarchy redditor redditor-mode redittor-mode redittor org-gcal tree-mode htmlize org-mu4e mu4e-alert mu4e js2-mode indium fontawesome ess adaptive-wrap ac-dabbrev pdf-tools go-guru go-autocomplete graphviz-dot-mode tickscript-mode ob-http pdb-mode undo-tree kubernetes yaml-mode waher-theme use-package solarized-theme smartscan smartparens org-journal ob-go monokai-theme markdown-mode magit ledger-mode jedi highlight-indent-guides helm-swoop helm-company gotest flymd flycheck fill-column-indicator exec-path-from-shell elfeed-web elfeed-org dockerfile-mode docker cyberpunk-theme ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -54,7 +55,7 @@
  '(elfeed-search-title-face ((t (:foreground "#c678dd" :height 1.2))))
  '(go-guru-hl-identifier-face ((t (:underline t))))
  '(highlight ((t (:background "color-237"))))
- '(highlight-indent-guides-character-face ((t (:foreground "gray73"))))
+ '(highlight-indent-guides-character-face ((t (:foreground "gray5"))))
  '(ledger-font-xact-highlight-face ((t (:background "color-237"))))
  '(magit-diff-added ((t (:foreground "#22aa22"))))
  '(magit-diff-added-highlight ((t (:foreground "#22aa22"))))
