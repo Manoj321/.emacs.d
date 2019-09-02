32b2370925a50438acf4fffc9ba9e4e164d1f0d9
Manoj321 <manojm.321@gmail.com>
2019-09-01 18:56:20 -0700

    Adding whole of elpa in case some packages go missing

---
+++ elpa/kubel-20190627.246/kubel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kubel" "20190627.246" "extension for controlling Kubernetes with limited permissions" '((transient "0.1.0") (emacs "25.3")) :commit "4e5009540bb3305c9a0a298ffff25b933d6bbbdf" :keywords '("kubernetes" "k8s" "tools" "processes") :authors '(("Adrien Brochard")) :maintainer '("Adrien Brochard") :url "https://github.com/abrochard/kubel")
